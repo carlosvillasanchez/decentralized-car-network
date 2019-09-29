44fc303c7c48bd398102682faf8610680d11a164
tormey <torstme@stud.ntnu.no>
2019-09-29 20:42:05 +0200

    Add client-peerster communication

--- client/main.go
+++ client/main.go
@@ -2,12 +2,35 @@ package main
 
 import (
 	"flag"
-	"fmt"
+	"log"
+	"net"
 	//"github.com/TorsteinMeyer/Peerster/messaging"
 )
 
-func main() {
-	UIPort := flag.String("UIPort", "8080", "port for the UI client (default '8080'")
+type PeersterClient struct {
+	UIPort string
+	msg    string
+}
+
+func (client PeersterClient) connect() (net.Conn, error) {
+	return net.Dial("udp", "127.0.0.1:"+client.UIPort)
+}
+
+func createClient() PeersterClient {
+	UIPort := flag.String("UIPort", "10000", "port for the UI client (default '8080'")
 	msg := flag.String("msg", "Default message", "message to be sent")
-	fmt.Printf("UIPort: %q, msg: %q, test", UIPort, msg)
+	flag.Parse()
+	return PeersterClient{
+		UIPort: *UIPort,
+		msg:    *msg,
+	}
+}
+
+func main() {
+	client := createClient()
+	conn, err := client.connect()
+	if err != nil {
+		log.Fatalf("Fatal error: PeersterClient was unable to connect. %s", err)
+	}
+	conn.Write([]byte(client.msg))
 }
--- main.go
+++ main.go
@@ -3,29 +3,94 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/dedis/protobuf"
+	"github.com/tormey97/Peerster/messaging"
 	"log"
-	"strings"
 	"net"
-	//"github.com/TorsteinMeyer/Peerster/messaging"
+	"strings"
+)
+
+type Origin int
+
+const (
+	Client Origin = iota
+	Server
 )
 
 type Peerster struct {
-	uiPort string
+	UIPort     string
 	gossipAddr string
 	knownPeers []string
-	name string
-	simple bool
+	name       string
+	simple     bool
 }
 
-func createPeerster() Peerster{
-	uiPort := flag.String("UIPort", "8080", "the port the client uses to communicate with peerster")
+func (peerster Peerster) String() string {
+	return fmt.Sprintf(
+		"UIPort: %s, gossipAddr: %s, knownPeers: %s, name: %s, simple: %s", peerster.UIPort, peerster.gossipAddr, peerster.knownPeers, peerster.name, peerster.simple)
+}
+
+func (peerster Peerster) listen(origin Origin) {
+	var addr string
+	switch origin {
+	case Client:
+		addr = "127.0.0.1:" + peerster.UIPort
+	case Server:
+		addr = peerster.gossipAddr
+	}
+	conn, err := net.ListenPacket("udp", addr)
+	if err != nil {
+		log.Fatalf("Error: could not listen. Origin: %s, error: %s", origin, err)
+	}
+	for {
+		buffer := make([]byte, 1024)
+		conn.ReadFrom(buffer)
+		var packet messaging.GossipPacket
+		switch origin {
+		case Client:
+			fmt.Println("CLIENT MESSAGE " + string(buffer))
+			packet = messaging.GossipPacket{Simple: peerster.createMessage(string(buffer))}
+			peerster.sendToKnownPeers(packet)
+		case Server:
+			fmt.Println("SERVER MESSAGE" + string(buffer))
+		}
+	}
+}
+
+func (peerster Peerster) createMessage(msg string) *messaging.SimpleMessage {
+	return &messaging.SimpleMessage{
+		OriginalName:  peerster.name,
+		RelayPeerAddr: peerster.gossipAddr,
+		Contents:      msg,
+	}
+}
+
+// Sends a GossipPacket to all known peers
+func (peerster Peerster) sendToKnownPeers(packet messaging.GossipPacket) error {
+	for i := range peerster.knownPeers {
+		peer := peerster.knownPeers[i]
+		conn, err := net.Dial("udp", peer)
+		if err != nil {
+			return err
+		}
+		packetBytes, err := protobuf.Encode(packet)
+		if err != nil {
+			return err
+		}
+		conn.Write(packetBytes)
+	}
+	return nil
+}
+
+func createPeerster() Peerster {
+	UIPort := flag.String("UIPort", "8080", "the port the client uses to communicate with peerster")
 	gossipAddr := flag.String("gossipAddr", "127.0.0.1:5000", "the address of the peerster")
 	name := flag.String("name", "nodeA", "the name of the node")
 	peers := flag.String("peers", "", "known peers")
 	simple := flag.Bool("simple", true, "simple mode")
 	flag.Parse()
 	return Peerster{
-		uiPort:     *uiPort,
+		UIPort:     *UIPort,
 		gossipAddr: *gossipAddr,
 		knownPeers: strings.Split(*peers, ","),
 		name:       *name,
@@ -33,26 +98,9 @@ func createPeerster() Peerster{
 	}
 }
 
-func (peerster *Peerster) listen() {
-	l, err := net.Listen("udp", fmt.Sprintf(":%s", peerster.uiPort))
-	if err != nil {
-		log.Fatalf("Error: could not listen: %s", err);
-	}
-	for {
-		conn, err := l.Accept();
-		if err != nil {
-			log.Fatalf("Error: l.Accept() failed: %s", err)
-		}
-		l, err := conn.Read([]byte{})
-		if err != nil {
-			log.Fatalf("Error: conn.Read() failed: %s", err)
-		}
-		println(l)
-	}
-}
-
 func main() {
 	peerster := createPeerster()
-	peerster.listen()
-	fmt.Printf("uiPort: %q, gossipAddr: %q", peerster.uiPort, peerster.gossipAddr)
+	fmt.Println(peerster.String())
+	peerster.listen(Client)
+
 }
